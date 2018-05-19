00c4370b2c4bba39fe6e9f6a8e07466a4c01d465
EGBoldyr <evgenboldyr@gmail.com>
2018-05-19 17:44:55 +0300

    BookDAO && BookDAOImpl

---
+++ Pro005_Examples/src/main/java/hibernate/lesson13/dao/BookDAO.java
@@ -0,0 +1,19 @@
+package hibernate.lesson13.dao;
+
+import hibernate.lesson13.entity.Book;
+
+import java.util.List;
+
+public interface BookDAO {
+
+    Long create(Book book);
+
+    Book read(Long id);
+
+    boolean update(Book book);
+
+    boolean delete(Book book);
+
+    List<Book> findAll();
+
+}
