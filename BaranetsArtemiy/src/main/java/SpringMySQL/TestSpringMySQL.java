d09ee010d665ea8f4035cd92c9d0eb466d1dab79
Artemiy <test>
2018-05-27 16:36:33 +0300

    init

---
+++ BaranetsArtemiy/src/main/java/SpringMySQL/TestSpringMySQL.java
@@ -0,0 +1,21 @@
+package SpringMySQL;
+
+import SpringMySQL.service.RobotService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Calendar;
+
+public class TestSpringMySQL {
+
+    public static void main(String[] args){
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("SpringMySQL/main-config.xml");
+
+        RobotService service = context.getBean(RobotService.class);
+
+        service.create("R2D2", Calendar.getInstance().getTime(),1000.00);
+
+    }
+
+}
