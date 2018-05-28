385e7a2e72d1a5a2bfb6791076b8ecc271f1ac59
Artemiy <test>
2018-05-28 19:08:48 +0300

    heh

--- Baranets_Artemiy_WEB/src/main/java/AppStart.java
+++ Baranets_Artemiy_WEB/src/main/java/AppStart.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class AppStart {
 
 
-
     public static void main(String[] args){
         SpringApplication.run(AppStart.class,args);
     }
