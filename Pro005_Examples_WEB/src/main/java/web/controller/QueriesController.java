1a2f55dda2141cda058049aa15fceeaa23cbd028
EGBoldyr <evgenboldyr@gmail.com>
2018-06-16 16:17:03 +0300

    Maven Settings

---
+++ Pro005_Examples_WEB/src/main/java/web/controller/QueriesController.java
@@ -0,0 +1,31 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import web.dao.PersonRepository;
+import web.domain.Person;
+
+@Controller
+public class QueriesController {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @RequestMapping(
+            value = "/person/create",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createPerson(
+            @RequestParam("name") String name,
+            @RequestParam("surname") String surname,
+            @RequestParam("age") Integer age) {
+        personRepository.saveAndFlush(new Person(name, surname, age));
+    }
+}
