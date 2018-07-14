95a099b8d8733795e30b57821f5fdc11ea7b221a
EGBoldyr <evgenboldyr@gmail.com>
2018-07-14 17:48:44 +0300

    Maven Settings

--- Pro005_Examples_REST_Client/src/main/java/com/rest/client/config/FeignMappingConfig.java
+++ Pro005_Examples_REST_Client/src/main/java/com/rest/client/config/FeignMappingConfig.java
@@ -1,14 +1,17 @@
 package com.rest.client.config;
 
+import com.rest.client.feign.RestApiFeignClient;
 import feign.Feign;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.web.WebMvcRegistrations;
 import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
+@EnableFeignClients(clients = { RestApiFeignClient.class })
 @ConditionalOnClass({ Feign.class })
 public class FeignMappingConfig {
 
