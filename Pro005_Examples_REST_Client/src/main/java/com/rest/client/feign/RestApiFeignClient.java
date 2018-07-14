a47be0dd007b7a25bb0ab1146eba9ad3a35fdfa6
EGBoldyr <evgenboldyr@gmail.com>
2018-07-14 17:57:45 +0300

    Maven Settings

--- Pro005_Examples_REST_Client/src/main/java/com/rest/client/feign/RestApiFeignClient.java
+++ Pro005_Examples_REST_Client/src/main/java/com/rest/client/feign/RestApiFeignClient.java
@@ -3,9 +3,10 @@ package com.rest.client.feign;
 import com.rest.api.endpoint.StudentsEndpoint;
 import com.rest.client.config.FeignClientConfig;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 
 @FeignClient(
-        value = "Rest API",
+        value = "Rest-API",
         url   = "http://localhost:15000",
         configuration = FeignClientConfig.class
 )
